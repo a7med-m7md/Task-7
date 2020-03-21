852f2598a783b549641cdbbea109613d6b377d3a
new00hurt <new00hurt@gmail.com>
2020-03-21 20:57:38 +0200

    FirstCommit

---
+++ src/task7/Task7.java
@@ -0,0 +1,15 @@
+
+package task7;
+
+
+public class Task7 {
+
+
+    public static void main(String[] args) {
+        double raduis = 7.5;
+        double PI = 22.0/7;
+        System.out.println("perimeter is = " + (2*PI*raduis));
+        System.out.println("Area is = " + (PI*raduis*raduis));
+    }
+    
+}
